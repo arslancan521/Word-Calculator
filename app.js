746e26a89754cfdf44042f83145b5f13607aa0dd
CAN ARSLAN <96886391+arslancan521@users.noreply.github.com>
2022-08-29 22:57:05 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+
+var text = document.querySelector(".text");
+var words = document.querySelector(".words");
+var characters = document.querySelector(".characters");
+text.addEventListener("input", function() {
+    characters.textContent = text.value.length;
+    var txt = text.value.trim();
+    words.textContent = txt.split(/\s+/).filter(function(item){ return item} ).length
+});
+
