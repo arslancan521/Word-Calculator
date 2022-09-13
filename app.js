6943b0370588aac52e4a32b9bbac3f01ae995fab
CAN ARSLAN <96886391+arslancan521@users.noreply.github.com>
2022-09-13 16:05:05 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,5 +6,6 @@ text.addEventListener("input", function() {
     characters.textContent = text.value.length;
     var txt = text.value.trim();
     words.textContent = txt.split(/\s+/).filter(function(item){ return item} ).length
+    
 });
 
